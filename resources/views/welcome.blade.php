3f97868de42fbe5c1fffb03d23fdbf3d4f0aa76c
TrisHero-it <minhtri204zx@gmail.com>
2024-11-21 18:14:45 +0700

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,13 @@
-<?php
-
-echo '
-[
-    {
-        "domain": ".netflix.com",
-        "expirationDate": 1761809504.918996,
-        "hostOnly": false,
-        "httpOnly": true,
-        "name": "SecureNetflixId",
-        "path": "/",
-        "sameSite": "strict",
-        "secure": true,
-        "session": false,
-        "storeId": null,
-        "value": "v%3D3%26mac%3DAQEAEQABABTCjgoc22Q9MsLr9aieiVm2B4gfLjepkXY.%26dt%3D1730273505249"
-    },
-    {
-        "domain": ".netflix.com",
-        "expirationDate": 1761809504.919024,
-        "hostOnly": false,
-        "httpOnly": true,
-        "name": "NetflixId",
-        "path": "/",
-        "sameSite": "lax",
-        "secure": true,
-        "session": false,
-        "storeId": null,
-        "value": "v%3D3%26ct%3DBgjHlOvcAxKCAycnY9VRMTiniztpsv-F3_d5LG3iy6yH8g1qNSZJqCEzNEEfDg021_sBQyL6h0ub13kby54NuUOGohVCAPX6Go0z5O7rVIq3wVR5aCi1wEkzvgLdNt0m2jJzUp0lpzfvjoaxEuTeW_QomqtUqgcday1GhY2DjQxlFubCDdRCC24QKCiB0yNhJWZY1yFCr-y-5S08zitfAsoGFD-pJoJK2ZLQ4XSbzb6XcgCOx2iv_TaxANdjHL5UiyiXkvb2FbEsUhyQAG4hfhcOBZm8NskifGLX3vDacvMXvKJgy76xHxpcXVG6Ily7SLHnl7_wX4O-g3dOpdYwLYYsMbTae34Y1S1HXIwiGYkD6P5QpwmFtwNbjD876FqK329fuEdAC25s6m5xsl5gVIpnJquw3WtKuVEb5L8mNBAlGJ9rsj7Q_WrCJDoTPXKmQU7sSLqOcibcZwkM0eilGaFTrtOSzgrVaax74oSHzG2hItXqBBOE--4t5-0MGPVIIIScxukNoVY23nqRGAYiDgoM8XEKE3l6oZO8EKb0%26ch%3DAQEAEAABABRZhNBpFy3xmyIOBzBnYPeibYHnIARujJE."
-    }
-]
-';
-
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Đây là trang chủ
+</body>
+</html>
