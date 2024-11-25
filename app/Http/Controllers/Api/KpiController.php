1de073197fba04fdd551fb56de7b4fc55d115979
TrisHero-it <minhtri204zx@gmail.com>
2024-11-25 14:57:49 +0700

    2

--- app/Http/Controllers/Api/KpiController.php
+++ app/Http/Controllers/Api/KpiController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Account;
+use App\Models\AccountProfile;
 use App\Models\HistoryMoveTask;
 use App\Models\Kpi;
+use App\Models\Stage;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -13,14 +15,17 @@ use Illuminate\Support\Facades\DB;
 class KpiController extends Controller
 {
     public function index(Request $request) {
-        $accounts = Account::all();
-        foreach ($accounts as $account) {
-            $account['CompletedOnTime'] = Kpi::query()->where('status', 1)->where('account_id', $account->id)->get()->count();
-            $account['CompletedLate'] = Kpi::query()->where('status', 0)->where('account_id', $account->id)->get()->count();
-            $account['InProgress'] = Task::query()->where('account_id', $account->id)->where('expired', '>',now())->get()->count();
-            $account['Overdue'] = Task::query()->where('account_id', $account->id)->where('expired', '<',now())->get()->count();
+        $stages = Stage::query()->where('workflow_id', $request->workflow_id)->get();
+        $accounts = Account::query()->select('id')->get();
+        foreach ($stages as $stage) {
+            foreach ($accounts as $account) {
+                $kpi =  Kpi::query()->where('stage_id', $stage->id)->where('account_id', $account->id)->get()->count();
+                $account['CompletedOnTime'] = $kpi;
+                $account['full_name'] = AccountProfile::query()->where('email', $account->id)->first()->full_name;
+            }
+            $stage['accounts'] = $accounts;
         }
 
-        return response()->json($accounts);
+        return response()->json($stages);
     }
 }
