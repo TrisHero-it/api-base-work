04b77fc6d5b6c2b0fcc6d3da611084196d5d0db3
TrisHero-it <minhtri204zx@gmail.com>
2024-11-25 15:51:25 +0700

    2

--- app/Http/Controllers/Api/KpiController.php
+++ app/Http/Controllers/Api/KpiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Account;
 use App\Models\AccountProfile;
+use App\Models\AccountWorkflow;
 use App\Models\HistoryMoveTask;
 use App\Models\Kpi;
 use App\Models\Stage;
@@ -16,9 +17,9 @@ class KpiController extends Controller
 {
     public function index(Request $request) {
         $stages = Stage::query()->where('workflow_id', $request->workflow_id)->where('index', '!=', '1')->where('index', '!=', '0')->orderBy('index', 'desc')->get();
-        $accounts = Account::query()->select('id')->get();
+        $accounts = AccountWorkflow::query()->select('id', 'account_id')->where('workflow_id', $request->workflow_id)->get();
             foreach ($accounts as $account) {
-                $account['full_name'] = AccountProfile::query()->where('email', $account->id)->value('full_name');
+                $account['full_name'] = AccountProfile::query()->where('email', $account->account_id)->value('full_name');
                 foreach ($stages as $stage) {
                     $account[$stage->name] = Kpi::query()->where('stage_id', $stage->id)->where('account_id', $account->id)->get()->count();
                 }
