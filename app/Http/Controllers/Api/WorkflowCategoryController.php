0ab0cf0b1f1ce5b61cdb5a8c9455027a9f359d68
TrisHero-it <minhtri204zx@gmail.com>
2025-01-23 16:16:23 +0700

    2

--- app/Http/Controllers/Api/WorkflowCategoryController.php
+++ app/Http/Controllers/Api/WorkflowCategoryController.php
@@ -20,7 +20,7 @@ class WorkflowCategoryController extends Controller
 
     public function index()
     {
-        $categories = WorkflowCategory::query()->with('workflows')->get();
+        $categories = WorkflowCategory::query()->get();
         $workflows = AccountWorkflow::where('account_id', Auth::id())->get();
         $arrWorkflowId = [];
         foreach ($workflows as $workflow) {
@@ -30,13 +30,13 @@ class WorkflowCategoryController extends Controller
         $arrCategoryId = $categories->pluck('id');
         $members =  AccountWorkflowCategory::query()->whereIn('workflow_category_id', $arrCategoryId)->with(['account'])->get();
         foreach ($categories as $category) {
-            // $category['workflows'] = $workflows->where('workflow_category_id', $category->id);
             $arrMembers = [];
             $members2 = $members->where('workflow_category_id', $category->id);
             foreach ($members2 as $member) {
                 $arrMembers[]  = $member->account;
             }
             $category['members'] = $arrMembers;
+            $category['workflows'] = $workflows->where('workflow_category_id', $category->id);
         }
 
         return response()->json($categories);
