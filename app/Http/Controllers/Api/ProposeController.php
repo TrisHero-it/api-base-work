013fc26524cda4272d910a45bff93ff3c87c45d3
TrisHero-it <minhtri204zx@gmail.com>
2025-01-24 08:53:35 +0700

    2

--- app/Http/Controllers/Api/ProposeController.php
+++ app/Http/Controllers/Api/ProposeController.php
@@ -45,7 +45,7 @@ class ProposeController extends Controller
                 $b = Attendance::whereDate('checkin', $a)->first();
                 if ($b != null) {
                     $propose['old_check_in'] = $b->checkin;
-                    $proposes['old_check_out'] = $b->checkout;
+                    $proposes['old_check_out'] = $b->checkout ?? 'chưa check out';
                 }
             }
             unset($propose['date_holidays']);
