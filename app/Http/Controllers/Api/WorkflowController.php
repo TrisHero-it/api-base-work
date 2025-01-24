2d7dad1b2fab8c911fbc145571e828d273063150
TrisHero-it <minhtri204zx@gmail.com>
2025-01-24 10:24:45 +0700

    2

--- app/Http/Controllers/Api/WorkflowController.php
+++ app/Http/Controllers/Api/WorkflowController.php
@@ -35,7 +35,7 @@ class WorkflowController extends Controller
             $query = Workflow::query();
         }
 
-        if (!Auth::user()->isSeniorAdmin) {
+        if (!Auth::user()->isSeniorAdmin()) {
             $query = $query->whereIn('id', $arrWorkflowId);
         }
 
