33c98920650d24c07d6816790a5af7f1043b5899
TrisHero-it <minhtri204zx@gmail.com>
2025-02-06 13:58:41 +0700

    2

--- app/Http/Controllers/Api/ProposeController.php
+++ app/Http/Controllers/Api/ProposeController.php
@@ -61,7 +61,7 @@ class ProposeController extends Controller
         $propose = Propose::with(['account', 'date_holidays', 'propose_category'])
         ->findOrFail($id);
         $a = explode(' ', $propose->start_time)[0];
-        $b = Attendance::whereDate('checkin', $a)->first();
+        $b = Attendance::whereDate('checkin', $a)->where('account_id', $propose->account_id)->first();
         if ($b != null) {
             $propose['old_check_in'] = $b->checkin;
             $propose['old_check_out'] = $b->checkout ?? null;
