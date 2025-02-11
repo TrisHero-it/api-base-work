eae1feb77d5cbf22fba0c85220864e9229194c72
TrisHero-it <minhtri204zx@gmail.com>
2025-02-11 11:38:24 +0700

    2

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,6 @@ use App\Http\Controllers\Api\ProposeController;
 use App\Http\Controllers\Api\ProposeCategoryController;
 use App\Http\Controllers\ScheduleWorkController;
 use Illuminate\Support\Facades\Route;
-use Jenssegers\Agent\Agent;
 
 Route::post('login', [LoginController::class, 'store'])->name('api.login.store');
 
@@ -35,12 +34,9 @@ Route::apiResource('accounts', AccountController::class);
 
 Route::post('send_email', [\App\Http\Controllers\Api\EmailController::class, 'sendEmail'])->name('api.email.send');
 Route::get('/test', function () {
-    $agent = new Agent();
-    $isMobile = $agent->isMobile();
     $ip = request()->ip();
     return response()->json([
         'ip' => $ip,
-        'isMobile' => $isMobile
     ]);
 });
 
