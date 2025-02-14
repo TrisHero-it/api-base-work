2198b0c05eed6145509addf26e4e45ddc8d61202
TrisHero-it <minhtri204zx@gmail.com>
2025-02-14 11:48:36 +0700

    2

--- app/Http/Controllers/ScheduleWorkController.php
+++ app/Http/Controllers/ScheduleWorkController.php
@@ -38,7 +38,7 @@ class ScheduleWorkController extends Controller
                                 ->where(function ($subSubQuery) use ($date) {
                                     $subSubQuery->whereDate('expired', '>=', $date);
                                     // Nếu $date lớn hơn ngày hiện tại, không lấy task có expired là NULL
-                                    if ($date > now()->toDateString()) {
+                                    if (Carbon::parse($date)->format('Y-m-d') > now()->toDateString()) {
                                         $subSubQuery->whereNotNull('expired');
                                     } else {
                                         $subSubQuery->orWhereNull('expired');
