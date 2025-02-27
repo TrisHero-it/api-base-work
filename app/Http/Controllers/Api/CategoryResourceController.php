2da9bcdca0f449d0e42123fe6437ce1c73e94eab
TrisHero-it <minhtri204zx@gmail.com>
2025-02-27 16:23:33 +0700

    2

--- app/Http/Controllers/Api/CategoryResourceController.php
+++ app/Http/Controllers/Api/CategoryResourceController.php
@@ -5,14 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\CategoryResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryResourceController extends Controller
 {
     public function index()
     {
-        $categoryResources = CategoryResource::with(['resources' => function ($query) {
-            $query->with('members', 'receivers');
-        }])->get();
+        $categoryResources = CategoryResource::with([
+            'resources' => function ($query) {
+                $query->whereHas('members', function ($q) {
+                   if(!Auth::user()->isSeniorAdmin()){
+                    $q->where('user_id', Auth::id());
+                   }
+                })->with('members', 'receivers');
+            }
+        ])->get();
 
         return response()->json($categoryResources);
     }
