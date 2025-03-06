75145471b3ecad8d3addc809fd8ff53e7e1649cf
TrisHero-it <minhtri204zx@gmail.com>
2025-03-06 23:43:33 +0700

    tắt check startDate holiday

--- app/Http/Controllers/Api/ProposeController.php
+++ app/Http/Controllers/Api/ProposeController.php
@@ -124,14 +124,14 @@ class ProposeController extends Controller
             }
         }
         $numberHoliDay = 0;
-        if ($request->filled('holiday')) {
-            $request->validate([
-                'holiday.start_date' => ['required', 'date_format:Y-m-d'],
-            ], [
-                'holiday.start_date.required' => 'Vui lòng nhập ngày bắt đầu.',
-                'holiday.start_date.date_format' => 'Ngày bắt đầu phải có định dạng YYYY-MM-DD.',
-            ]);
-        }
+        // if ($request->filled('holiday')) {
+        //     $request->validate([
+        //         'holiday.start_date' => ['required', 'date_format:Y-m-d'],
+        //     ], [
+        //         'holiday.start_date.required' => 'Vui lòng nhập ngày bắt đầu.',
+        //         'holiday.start_date.date_format' => 'Ngày bắt đầu phải có định dạng YYYY-MM-DD.',
+        //     ]);
+        // }
         if ($request->name == "Nghỉ có hưởng lương") {
             foreach ($request->holiday as $date2) {
                 $startDate = Carbon::parse($date2['start_date']);
